ed74359deb5de38cff71010d1b8a837d8be7fee8
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-11 11:45:33 +0530

    refactor(apps/consumer): moved status bar overlay code from pages/Home.tsx to App.tsx

--- apps/consumer/src/pages/Home.tsx
+++ apps/consumer/src/pages/Home.tsx
@@ -5,12 +5,9 @@ import {
 	IonTitle,
 	IonToolbar,
 } from "@ionic/react";
-import { StatusBar } from "@capacitor/status-bar";
 
 import ExploreContainer from "../components/ExploreContainer";
 import "./Home.css";
-
-StatusBar.setOverlaysWebView({ overlay: false }).catch(console.error);
 
 const Home: React.FC = () => {
 	return (
