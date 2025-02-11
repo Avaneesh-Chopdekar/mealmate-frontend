14240500f34992792b3ca4b381f18631859c2bd0
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-11 11:41:51 +0530

    feat: added rider app

---
+++ apps/rider/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.mealmate.rider",
+  appName: "MealMate for Riders",
+  webDir: "dist",
+};
+
+export default config;
