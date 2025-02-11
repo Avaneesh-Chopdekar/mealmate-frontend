552d77f18e4f1c6ed1b32ee273dc645ea97554dc
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-11 11:51:42 +0530

    feat: created restaurant app

---
+++ apps/restaurant/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.mealmate.restaurant",
+  appName: "MealMate for Restaurants",
+  webDir: "dist",
+};
+
+export default config;
