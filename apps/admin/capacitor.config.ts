35730d78bb0f8e79e5753fa9c3d6ed533af7ce48
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-11 12:01:16 +0530

    feat: created admin app

---
+++ apps/admin/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.mealmate.admin",
+  appName: "MealMate for Admins",
+  webDir: "dist",
+};
+
+export default config;
